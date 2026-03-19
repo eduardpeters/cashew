aa07ccc6ef8e9a22565aa77be030e8c712dd13b0
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2026-03-19 18:30:32 +0100

    scaffold tcp server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, Cashew!")
+	listener, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		log.Fatal("Error listening:", err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+	if err != nil {
+		log.Printf("Read error %v", err)
+		return
+	}
+
+	ackMsg := strings.ToUpper(strings.TrimSpace(message))
+	response := fmt.Sprintf("ACK: %s\n", ackMsg)
+
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		log.Printf("Server write error: %v", err)
+	}
 }
