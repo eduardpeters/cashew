f6606f6eb40dfe5abb68f853c3d598cb3af08f84
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2026-03-30 21:17:16 +0200

    allow for setting key expiration

--- internal/store/store.go
+++ internal/store/store.go
@@ -3,12 +3,15 @@ package store
 import (
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/eduardpeters/cashew/internal/resp"
 )
 
 type StoredValue struct {
-	value resp.CashewValue
+	value   resp.CashewValue
+	expiry  time.Time
+	expires bool
 }
 
 type Store struct {
@@ -30,7 +33,24 @@ func (s *Store) Set(key, value resp.BulkString) error {
 		return fmt.Errorf("key value is not string: %v", v)
 	}
 
-	s.store[k] = StoredValue{value: value}
+	s.store[k] = StoredValue{value: value, expires: false}
+
+	return nil
+}
+
+func (s *Store) SetWithExpiryMillis(key, value resp.BulkString, expiryMillis int) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	v := key.GetValue()
+	k, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("key value is not string: %v", v)
+	}
+
+	expiry := time.Now().Add(time.Millisecond * time.Duration(expiryMillis))
+
+	s.store[k] = StoredValue{value: value, expiry: expiry, expires: true}
 
 	return nil
 }
@@ -46,6 +66,11 @@ func (s *Store) Get(key resp.BulkString) (resp.CashewValue, error) {
 	}
 	stored, ok := s.store[k]
 	if !ok {
+		return resp.NewNull()
+	}
+
+	if stored.expires && stored.expiry.Before(time.Now()) {
+		delete(s.store, k)
 		return resp.NewNull()
 	}
 
--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -2,6 +2,8 @@ package store_test
 
 import (
 	"testing"
+	"testing/synctest"
+	"time"
 
 	"github.com/eduardpeters/cashew/internal/resp"
 	"github.com/eduardpeters/cashew/internal/store"
@@ -14,6 +16,28 @@ func TestSetNewValue(t *testing.T) {
 	s := store.NewStore()
 
 	err := s.Set(key, value)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+
+	stored, err := s.Get(key)
+	if err != nil {
+		t.Fatalf("Unexpected error %v", err)
+	}
+
+	if stored.GetValue() != value.GetValue() {
+		t.Errorf("incorrect stored value want %q, got %q", value.GetValue(), stored.GetValue())
+	}
+}
+
+func TestSetWithExpiryMillis(t *testing.T) {
+	key := mustNewBulkString(t, "name")
+	value := mustNewBulkString(t, "john")
+	expireMilliseconds := 5000
+
+	s := store.NewStore()
+
+	err := s.SetWithExpiryMillis(key, value, expireMilliseconds)
 	if err != nil {
 		t.Errorf("Unexpected error %v", err)
 	}
@@ -58,6 +82,48 @@ func TestGetValue(t *testing.T) {
 	}
 }
 
+func TestGetValueWithinExpiry(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+
+		s := store.NewStore()
+		key := "name"
+		value := "john"
+		mustSetInStoreWithExpiry(t, s, key, value, 5000)
+
+		time.Sleep(time.Second * 2)
+
+		stored, err := s.Get(mustNewBulkString(t, key))
+		if err != nil {
+			t.Fatalf("Unexpected error %v", err)
+		}
+
+		got := stored.GetValue()
+		if got != value {
+			t.Errorf("incorrect stored value want %q, got %q", value, got)
+		}
+	})
+}
+
+func TestGetValueAfterExpiry(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		s := store.NewStore()
+		key := "name"
+		value := "john"
+		mustSetInStoreWithExpiry(t, s, key, value, 1000)
+
+		time.Sleep(time.Second * 2)
+
+		stored, err := s.Get(mustNewBulkString(t, key))
+		if err != nil {
+			t.Fatalf("Unexpected error %v", err)
+		}
+
+		if stored.GetValue() != nil {
+			t.Errorf("incorrect stored value want %v, got %v", nil, stored.GetValue())
+		}
+	})
+}
+
 func mustNewSimpleString(t testing.TB, s string) resp.SimpleString {
 	t.Helper()
 	v, err := resp.NewSimpleString(s)
@@ -79,6 +145,14 @@ func mustNewBulkString(t testing.TB, s string) resp.BulkString {
 func mustSetInStore(t testing.TB, s *store.Store, key, value string) {
 	t.Helper()
 	err := s.Set(mustNewBulkString(t, key), mustNewBulkString(t, value))
+	if err != nil {
+		t.Fatalf("store.Set(%q,%q): %v", key, value, err)
+	}
+}
+
+func mustSetInStoreWithExpiry(t testing.TB, s *store.Store, key, value string, expiryMillis int) {
+	t.Helper()
+	err := s.SetWithExpiryMillis(mustNewBulkString(t, key), mustNewBulkString(t, value), expiryMillis)
 	if err != nil {
 		t.Fatalf("store.Set(%q,%q): %v", key, value, err)
 	}
