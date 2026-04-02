ffd2d6d369c8c3548f7184e50e95c630391528fb
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2026-04-02 08:09:35 +0200

    test simple set EX seconds

--- internal/commands/set.go
+++ internal/commands/set.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/eduardpeters/cashew/internal/resp"
 	"github.com/eduardpeters/cashew/internal/store"
@@ -25,6 +26,30 @@ func HandleSet(s *store.Store, args []resp.CashewValue) (Result, error) {
 	value, ok := v.(resp.BulkString)
 	if !ok {
 		return Result{}, fmt.Errorf("argument not bulk string: %v", k)
+	}
+	if len(args) > 2 {
+		if len(args) < 4 {
+			return Result{}, errors.New("missing arguments")
+		}
+
+		opt := args[2]
+		_, ok := opt.(resp.BulkString)
+		if !ok {
+			return Result{}, fmt.Errorf("argument not bulk string: %v", k)
+		}
+		d := args[3]
+		_, ok = d.(resp.BulkString)
+		if !ok {
+			return Result{}, fmt.Errorf("argument not bulk string: %v", k)
+		}
+
+		expiration := time.Now().Add(time.Second * 1)
+		err := s.SetWithExpiry(key, value, expiration)
+		if err != nil {
+			return Result{}, err
+		}
+
+		return ResultOK(), nil
 	}
 
 	err := s.Set(key, value)
--- internal/commands/set_test.go
+++ internal/commands/set_test.go
@@ -2,6 +2,8 @@ package commands_test
 
 import (
 	"testing"
+	"testing/synctest"
+	"time"
 
 	"github.com/eduardpeters/cashew/internal/commands"
 	"github.com/eduardpeters/cashew/internal/resp"
@@ -69,6 +71,9 @@ func TestHandleSetInvalidArguments(t *testing.T) {
 		{"Fails on non bulk string arguments - all simple string",
 			[]resp.CashewValue{mustNewSimpleString(t, "key"), mustNewSimpleString(t, "value")},
 		},
+		{"Fails on missing expiry seconds",
+			[]resp.CashewValue{mustNewSimpleString(t, "key"), mustNewSimpleString(t, "value"), mustNewBulkString(t, "EX")},
+		},
 	}
 
 	for _, tt := range tests {
@@ -79,6 +84,53 @@ func TestHandleSetInvalidArguments(t *testing.T) {
 			if err == nil {
 				t.Errorf("Expected error for %q - got: %v", tt.input, err)
 			}
+		})
+	}
+}
+
+func TestHandleSetEx(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         []resp.CashewValue
+		expireSeconds int
+	}{
+		{"sets an expiry for 1 second",
+			[]resp.CashewValue{mustNewBulkString(t, "name"), mustNewBulkString(t, "juan"), mustNewBulkString(t, "EX"), mustNewBulkString(t, "1")},
+			1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			synctest.Test(t, func(t *testing.T) {
+				s := store.NewStore()
+
+				_, err := commands.HandleSet(s, tt.input)
+				if err != nil {
+					t.Fatalf("Unexpected error: %v", err)
+				}
+
+				key := tt.input[0].(resp.BulkString)
+				stored, err := s.Get(key)
+				if err != nil {
+					t.Fatalf("Unexpected error %v", err)
+				}
+
+				if stored.GetValue() != tt.input[1].GetValue() {
+					t.Fatalf("incorrect stored value want %q, got %q", tt.input[1].GetValue(), stored.GetValue())
+				}
+
+				time.Sleep(time.Second*time.Duration(tt.expireSeconds) + time.Millisecond)
+
+				stored, err = s.Get(key)
+				if err != nil {
+					t.Fatalf("Unexpected error %v", err)
+				}
+
+				if stored.GetValue() != nil {
+					t.Errorf("incorrect value after expiration want %v, got %q", nil, stored.GetValue())
+				}
+			})
 		})
 	}
 }
